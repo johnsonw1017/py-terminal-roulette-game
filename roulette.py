c6f35eb999c5b32a35b1eb32e36ca94b0f8cea3b
johnsonw1017@gmail.com <johns@192-168-1-104.tpgi.com.au>
2022-12-11 21:02:08 +1000

    created get_input function for user input handling

--- roulette.py
+++ roulette.py
@@ -1,6 +1,6 @@
 # Create string for the roulette game board
 
-def drawBoard():
+def draw_board():
     row_top1 = "+---" * 14 + "+"
     row_top2 = "+   " + "+---" * 13 + "+"
     row_top3 = "    " + "+---" * 12 + "+"
@@ -34,5 +34,39 @@ def drawBoard():
     print("    |  1-18 |  EVEN |  RED  | BLACK |  ODD  | 19-36 |")
     print(row_top3)
 
-drawBoard()
+#Input handling
+def get_input(prompt):
+    
+    user_input = input(prompt)
 
+    # when user inputs "board", "help" or "quit"
+    if user_input == "board":
+        draw_board()
+
+    if user_input == "help":
+        pass
+
+    if user_input == "quit":
+        raise KeyboardInterrupt
+
+    return user_input
+
+##  Game play loop
+def main_loop():
+    # Introduction and Board
+    print("Welcome to a game of Roulette! Let's start with 100 chips.")
+    draw_board()
+    print("Type the following at any time during the game:\nhelp - for instructions\nboard - to view board\nquit - to quit game")
+
+    user_input = ""
+    location = ""
+
+    #Ask player the positions to place chips
+    while user_input != 'done':
+        user_input = get_input("Where would you like to place your chips?")
+        location = user_input
+
+    #Ask how many chips to add at that location
+        user_input = get_input(f"How many chips to place at {location}?")
+
+    #Update variable chip_placement accordingly
