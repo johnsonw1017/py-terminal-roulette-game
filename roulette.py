f218629c4231531a615c98f4a5cfea157dac27cc
johnsonw1017@gmail.com <johns@192-168-1-104.tpgi.com.au>
2022-12-11 21:43:48 +1000

    created print_instruction function that prints user instructions when called

--- roulette.py
+++ roulette.py
@@ -27,12 +27,19 @@ def draw_board():
             else:
                 print(f"| {boardnum}", end="")
         
-        print(f"| r{row}|")
+        print(f"| R{row}|")
 
     print("    |     1-12      |     13-24     |     25-36     |")
     print(row_top3)
     print("    |  1-18 |  EVEN |  RED  | BLACK |  ODD  | 19-36 |")
     print(row_top3)
+
+#Player instructions
+def print_instructions():
+    print("    u\n  +---+ \nl | 1 | r\n  +---+ \n    d")
+    print("Use the above diagram to add chips to edges of a number. For example u1 to add chips on top edge of number 1.")
+    print("To place chip at the corner between 3-4 numbers, use cN, where N is the largest of the numbers.")
+    print("For the remainder, add chips as appeared on the board.")
 
 #Input handling
 def get_input(prompt):
@@ -43,10 +50,10 @@ def get_input(prompt):
     if user_input == "board":
         draw_board()
 
-    if user_input == "help":
+    elif user_input == "help":
         pass
 
-    if user_input == "quit":
+    elif user_input == "quit":
         raise KeyboardInterrupt
 
     return user_input
@@ -58,15 +65,22 @@ def main_loop():
     draw_board()
     print("Type the following at any time during the game:\nhelp - for instructions\nboard - to view board\nquit - to quit game")
 
+    chip_amount = 100
     user_input = ""
     location = ""
 
-    #Ask player the positions to place chips
-    while user_input != 'done':
-        user_input = get_input("Where would you like to place your chips?")
-        location = user_input
+    try:
+        #Ask player the positions to place chips
+        while user_input != 'done':
+            user_input = get_input("Where would you like to place your chips?")
+            location = user_input
 
-    #Ask how many chips to add at that location
-        user_input = get_input(f"How many chips to place at {location}?")
+        #Ask how many chips to add at that location
+            user_input = get_input(f"How many chips to place at {location}?")
+    except KeyboardInterrupt:
+        print(f"Thanks for playing! You finished with {chip_amount} chips in your stack.")
+    
 
     #Update variable chip_placement accordingly
+
+main_loop()
